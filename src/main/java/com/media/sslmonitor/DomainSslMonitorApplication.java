dad6f5ccfe290d86bd4b4da0e8caa03ae9119bb9
Andrei Gabor <andreigabor36@gmail.com>
2025-08-05 15:43:29 +0300

    feat: First commit

---
+++ src/main/java/com/media/sslmonitor/DomainSslMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.media.sslmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainSslMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainSslMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/media/sslmonitor/DomainSslMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.media.sslmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainSslMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
